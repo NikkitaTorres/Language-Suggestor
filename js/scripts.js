3c374dcbd809ceac668bacf8746964c068449a02
NikkitaTorres <nikkitatorres@yahoo.com>
2023-10-20 10:25:05 -0700

    Add values to existing questions and produce a numbered result

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 function handleSelect(event) {
   event.preventDefault();
+  const Question1 = parseInt(document.getElementById("Q1").value);
+  const Question2 = parseInt(document.getElementById("Q2").value);
+  const Question3 = parseInt(document.getElementById("Q3").value);
+  const Question4 = parseInt(document.getElementById("Q4").value);
+  const Question5 = parseInt(document.getElementById("Q5").value);
 }
 
 window.addEventListener("load", function() {
-  const Question1 = parseInt(document.querySelector("Q1").value);
   document.getElementById("select-form").addEventListener("submit", handleSelect);
   let output = document.getElementById("output");
   document.getElementById("select-form").reset();
