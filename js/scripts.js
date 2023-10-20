2619bfc3d864251b69d976cfb9696616d83b5e8c
NikkitaTorres <nikkitatorres@yahoo.com>
2023-10-20 09:47:07 -0700

    Let reset function work through scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 function handleSelect(event) {
   event.preventDefault();
-  const selection = document.getElementById("Q1").value;
 }
 
 window.addEventListener("load", function() {
-  const Q1 = document.getElementById("Q1").value;
+  const Question1 = parseInt(document.querySelector("Q1").value);
   document.getElementById("select-form").addEventListener("submit", handleSelect);
   let output = document.getElementById("output");
   document.getElementById("select-form").reset();
