2ddb6a39d11f640dadffa1c8a51bbab84b5b5b38
NikkitaTorres <nikkitatorres@yahoo.com>
2023-10-20 15:46:32 -0700

    Fix bug where result totalling 8 was providing an incorrect resopnse

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@ window.addEventListener("load", function() {
       message = "Please fill out all questions for a result.";
     } else if (result >= 5 && result < 8) {
       message = "Python: An excerpt from coursera.org - \"A great language for developing websites and software, task automation, data analysis, and data visualization.\" This is a great beginner language and one that is sure to be useful! Checkout python.org/about/gettingstarted for more info!";
-    } else if (result > 8 && result <= 12) {
+    } else if (result >= 8 && result <= 12) {
       message = "Ruby: An excerpt from pluralsight.com - \"Used for building desktop applications, static websites, data processing services, and even automation tools.\" Ruby is a popular language and beginner friendly! You can read more about Ruby here: https://guides.rubyonrails.org";
     } else if (result > 12) {
       message = "C#: An excerpt from pluralsight.com - \"C# can be used to create a number of different programs and applications: mobile apps, desktop apps, cloud-based services, websites, enterprise software and games.\" Find out more about how to get started with learning C# here: https://www.w3schools.com/cs/cs_getstarted.php";
